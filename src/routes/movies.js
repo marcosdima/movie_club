4511a0ae5fed57bcbe57ef04d0a329b99f27aa70
marcosdima <marcosdimatteo.dev@gmail.com>
2024-05-06 15:40:24 -0300

    Setting routers

---
+++ src/routes/movies.js
@@ -0,0 +1,11 @@
+const moviesRouter = require('express').Router();
+
+moviesRouter.get('/', (req, res) => {
+    // TODO
+});
+
+moviesRouter.post('/', (req, res) => {
+    // TODO
+});
+
+module.exports = moviesRouter;
