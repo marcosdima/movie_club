b7a9582ec97c2a6f80968143c2fc9142e9bc6800
marcosdima <marcosdimatteo.dev@gmail.com>
2024-05-06 14:25:42 -0300

    Starting project

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express()
+
+app.get('/ping', (req, res) => {
+    res.send('pong');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
