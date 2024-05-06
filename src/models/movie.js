fe21409a33fa048ba4b244a0928d00013e2de9e0
marcosdima <marcosdimatteo.dev@gmail.com>
2024-05-06 15:40:35 -0300

    Defining models

---
+++ src/models/movie.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema({
+	title: {
+		type: String,
+		required: true,
+	},
+	director: {
+		type: String,
+		required: true,
+	},
+    description: String,
+    genres: [String],
+    comments: [
+        {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Comment'
+		}
+    ],
+    imageUrl: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+          validator: function(v) {
+            return /^(http|https):\/\/[^ "]+$/.test(v);
+          },
+          message: props => `${props.value} no es una URL válida!`
+        }
+    }
+});
+
+movieSchema.set('toJSON', {
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString();
+		delete returnedObject._id;
+		delete returnedObject.__v;
+	}
+});
+  
+module.exports = mongoose.model('Movie', movieSchema);
