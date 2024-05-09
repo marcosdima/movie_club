c94ca30595c086bf80d9ada95a5d99812d8e37ab
marcosdima <marcosdimatteo.dev@gmail.com>
2024-05-09 14:14:55 -0300

    Deleting passwordHash from toJSON

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -23,6 +23,7 @@ userScheme.set('toJSON', {
     returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id;
     delete returnedObject.__v;
+    delete returnedObject.passwordHash;
   }
 });
 
