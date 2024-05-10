3e242e30aa7e2239b7b7681a1640840d86f527d7
marcosdima <marcosdimatteo.dev@gmail.com>
2024-05-10 14:26:48 -0300

    New file 'app.js' - Exctract the app component to its own file and import it in index.js

---
+++ backend/src/app.js
@@ -0,0 +1,35 @@
+require('express-async-errors');
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+// Utils
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+const middleware = require('./utils/middleware');
+
+// Routers
+const moviesRouter = require('./routes/moviesRouter');
+const usersRouter = require('./routes/usersRouter');
+const groupRouter = require('./routes/groupRouter');
+const loginRouter = require('./routes/loginRouter');
+const activitiesRouter = require('./routes/activitiesRouter');
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB');
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message);
+  });
+
+app.use(express.json());
+app.use(middleware.tokenExtractor);
+app.use(middleware.userExtractor);
+app.use('/api/movies', moviesRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/groups', groupRouter);
+app.use('/api/login', loginRouter);
+app.use('/api/activities', activitiesRouter)
+
+module.exports = app;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,38 +1,8 @@
-require('express-async-errors');
-const express = require('express');
-const mongoose = require('mongoose');
-const app = express();
+const server = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-// Utils
-const config = require('./utils/config');
-const logger = require('./utils/logger');
-const middleware = require('./utils/middleware');
-
-// Routers
-const moviesRouter = require('./routes/moviesRouter');
-const usersRouter = require('./routes/usersRouter');
-const groupRouter = require('./routes/groupRouter');
-const loginRouter = require('./routes/loginRouter');
-const activitiesRouter = require('./routes/activitiesRouter');
-
-mongoose.connect(config.MONGODB_URI)
-  .then(() => {
-    logger.info('connected to MongoDB');
-  })
-  .catch((error) => {
-    logger.error('error connecting to MongoDB:', error.message);
-  });
-
-app.use(express.json());
-app.use(middleware.tokenExtractor);
-app.use(middleware.userExtractor);
-app.use('/api/movies', moviesRouter);
-app.use('/api/users', usersRouter);
-app.use('/api/groups', groupRouter);
-app.use('/api/login', loginRouter);
-app.use('/api/activities', activitiesRouter)
-
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+server.listen(config.PORT, () => {
+	logger.info(`Server running on port ${config.PORT}`)
+	logger.info(`URL: http://localhost:${config.PORT}/api/blogs`)
+})
