274bd5b2224d7607c92274dd349f63e4cbc87b6a
marcosdima <marcosdimatteo.dev@gmail.com>
2024-06-13 21:25:26 -0300

    mod: divide validCreation new: comment deletion

--- backend/src/routes/commentsRouter.js
+++ backend/src/routes/commentsRouter.js
@@ -6,17 +6,32 @@ const Comment = require('../models/comment');
 const checkModelStructure = require('../utils/middleware').checkModelStructure;
 
 // Middleware to valid if a comment can be created.
-const validCreation = async (req, res, next) => {
-  if (!req.user) return res.status(401).json({ error: 'token invalid' });
-
+const activityExists = async (req, res, next) => {
   const { activity: activityId } = req.body;
 
   const activity = await activitiesService.getActivityById(activityId);
   if (!activity) return res.status(404).json({ error: 'activity does not exist' });
+  req.activity = activity;
+  next();
+};
 
-  const { members } = await groupsService.getGroup(activity.group, false);
+const belongsToGroup = async (req, res, next) => {
+  if (!req.user) return res.status(401).json({ error: 'token invalid' });
+  const { id } = req.params;
+  const { activity } = req;
+  let groupId;
+
+  // If req.activity does not exists...
+  if (!activity) {
+    const comment = await commentsService.getCommentById(id);
+    if (!comment) return res.status(404).json({ error: "comment does not exist" }); 
+    groupId = comment.activity.group.toString();
+  } else groupId = activity.group.toString();
+
+  const { members } = await groupsService.getGroup(groupId, false);
   const { id: userId } = req.user;
   const membersIds = members.map((member) => member.toString());
+
   if (!membersIds.includes(userId)) return res.status(404).json({ error: "user don't belong to the activity group" });
 
   next();
@@ -27,9 +42,15 @@ commentsRouter.get('/:activityId', async (req, res) => {
   res.json(await commentsService.getComments(activityId));
 });
 
-commentsRouter.post('/', validCreation, checkModelStructure(Comment), async (req, res) => {
+commentsRouter.post('/', activityExists, belongsToGroup, checkModelStructure(Comment), async (req, res) => {
   const newComment = await commentsService.createComment(req.body);
   res.status(201).json(newComment);
+});
+
+commentsRouter.delete('/:id', belongsToGroup, async (req, res) => {
+  const { id } = req.params;
+  await commentsService.deleteComment(id);
+  res.json({ message: 'Comment deleted successfully!' });
 });
 
 module.exports = commentsRouter;
